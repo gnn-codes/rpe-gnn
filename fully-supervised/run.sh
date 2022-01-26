20dd69bb934d78dd2d994f654693e75184cb11c9
gnn-codes <98342947+gnn-codes@users.noreply.github.com>
2022-01-27 02:02:27 +0530

    Add files via upload

---
+++ fully-supervised/run.sh
@@ -0,0 +1,1 @@
+python -u main.py --dataset 'pubmed' --lr 0.2 --hidden_layers 8 --hidden_dim 64 --train_iter 200 --test_iter 10 --use_saved_model True  --l_range 3 --n_sample 3 --dropout 0.50 --wdecay 0.00005 --device 'cuda:0' 
